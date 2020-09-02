849eb847a6278583b6e217a3b95d8c751380b47e
YuyaHirako <hirako.yuuya0928.4620@gmail.com>
2020-09-02 21:24:14 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  root to: 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
